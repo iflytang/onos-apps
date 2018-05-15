786d5a9a2c87a28f7b7e5ba42f9610aae272c5bc
iflytang <iflytang@qq.com>
2018-05-15 09:53:13 +0800

    test flow mod for rebuilt floodlightpof: output_action

---
+++ pof-ovs-action-test-app/src/main/java/org/onosproject/test/action/AppComponent.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2018-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.test.action;
+
+import org.apache.felix.scr.annotations.*;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.floodlightpof.protocol.OFMatch20;
+import org.onosproject.floodlightpof.protocol.action.OFAction;
+import org.onosproject.floodlightpof.protocol.table.OFFlowTable;
+import org.onosproject.floodlightpof.protocol.table.OFTableType;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.device.DeviceAdminService;
+import org.onosproject.net.flow.*;
+import org.onosproject.net.flow.criteria.Criteria;
+import org.onosproject.net.flow.criteria.Criterion;
+import org.onosproject.net.flow.instructions.DefaultPofActions;
+import org.onosproject.net.flow.instructions.DefaultPofInstructions;
+import org.onosproject.net.table.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Skeletal ONOS application component.
+ */
+@Component(immediate = true)
+public class AppComponent {
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowTableStore flowTableStore;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowRuleService flowRuleService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected CoreService coreService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected DeviceAdminService deviceService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowTableStore tableStore;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowTableService flowTableService;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private ApplicationId appId;
+
+    private byte tableId;
+    private DeviceId deviceId;
+
+    // field_id
+    final short SIP = 1;
+
+    @Activate
+    protected void activate() {
+        appId = coreService.registerApplication("org.onosproject.test.action");
+        pofTestStart();
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        pofTestStop();
+    }
+
+    public void pofTestStart() {
+        log.info("org.onosproject.pof.test.action Started");
+        // deviceId = DeviceId.deviceId("pof:ffffffffcd0318d2");
+        deviceId = deviceService.getAvailableDevices().iterator().next().id();
+
+        // send flow table
+        tableId = sendPofFlowTable(deviceId);
+
+        // wait 1s
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // send flow rules
+        installOutputFlowRule(deviceId, tableId, "0a000001", 2);
+    }
+
+    public void pofTestStop() {
+        removeFlowTable(deviceId, tableId);
+        log.info("org.onosproject.test.action Stopped");
+    }
+
+    public byte sendPofFlowTable(DeviceId deviceId) {
+        byte tableId = (byte) tableStore.getNewGlobalFlowTableId(deviceId, OFTableType.OF_MM_TABLE);
+
+        OFMatch20 srcIP = new OFMatch20();
+        srcIP.setFieldId((short) 1);
+        srcIP.setFieldName("srcIP");
+        srcIP.setOffset((short) 204);
+        srcIP.setLength((short) 32);
+
+        ArrayList<OFMatch20> match20List = new ArrayList<>();
+        match20List.add(srcIP);
+
+        OFFlowTable ofFlowTable = new OFFlowTable();
+        ofFlowTable.setTableId(tableId);
+        ofFlowTable.setTableName("FirstEntryTable");
+        ofFlowTable.setMatchFieldList(match20List);
+        ofFlowTable.setMatchFieldNum((byte) 1);
+        ofFlowTable.setTableSize(32);
+        ofFlowTable.setTableType(OFTableType.OF_MM_TABLE);
+        ofFlowTable.setCommand(null);
+        ofFlowTable.setKeyLength((short) 32);
+
+        FlowTable.Builder flowTable = DefaultFlowTable.builder()
+                .withFlowTable(ofFlowTable)
+                .forTable(tableId)
+                .forDevice(deviceId)
+                .fromApp(appId);
+
+        flowTableService.applyFlowTables(flowTable.build());
+
+        log.info("table<{}> applied to device<{}> successfully.", tableId, deviceId.toString());
+
+        return tableId;
+    }
+
+    public void removeFlowTable(DeviceId deviceId, byte tableId) {
+        // will delete flow entries first, then delete flow tables
+        flowTableService.removeFlowTablesByTableId(deviceId, FlowTableId.valueOf(tableId));
+    }
+
+    public void installOutputFlowRule(DeviceId deviceId, byte tableId, String srcIP, int outport) {
+        // match
+        TrafficSelector.Builder trafficSelector = DefaultTrafficSelector.builder();
+        ArrayList<Criterion> matchList = new ArrayList<>();
+        matchList.add(Criteria.matchOffsetLength((short) SIP, (short) 208, (short) 32, srcIP, "ffffffff"));
+        trafficSelector.add(Criteria.matchOffsetLength(matchList));
+
+        // action
+        TrafficTreatment.Builder trafficTreamt = DefaultTrafficTreatment.builder();
+        List<OFAction> actions = new ArrayList<>();
+        OFAction action_output = DefaultPofActions.output((short) 0, (short) 0, (short) 0, outport).action();
+        actions.add(action_output);
+        trafficTreamt.add(DefaultPofInstructions.applyActions(actions));
+        log.info("action_output: {}.", actions);
+
+        // apply
+        long newFlowEntryId = flowTableStore.getNewFlowEntryId(deviceId, tableId);
+        FlowRule.Builder flowRule = DefaultFlowRule.builder()
+                                                   .forDevice(deviceId)
+                                                   .forTable(tableId)
+                                                   .withSelector(trafficSelector.build())
+                                                   .withTreatment(trafficTreamt.build())
+                                                   .withPriority(1)
+                                                   .withCookie(newFlowEntryId)
+                                                   .makePermanent();
+        flowRuleService.applyFlowRules(flowRule.build());
+
+        log.info("installOutputFlowRule: apply to deviceId<{}> tableId<{}>", deviceId.toString(), tableId);
+    }
+
+}
